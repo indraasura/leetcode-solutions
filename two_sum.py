2efdbe8a15fe4c0ca99a4d334d8f493e834bf535
Swarup Hegde <swaruphegde@gmail.com>
2020-05-28 18:23:17 +0530

    fix: two sum

--- two_sum.py
+++ two_sum.py
@@ -1,14 +1,12 @@
-input_list = [int(x) for x in input().split(' ')]
-target = int(input())
-
-elements = {}
-output_list = [0, 0]
-for i in input_list:
-    difference = target-i
-    if difference in elements:
-        output_list[0] = elements[difference]
-        output_list[1] = input_list.index(i)
-    else: 
-        elements[i] = input_list.index(i)
-
-print(output_list)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        output = [0,0]
+        for digit in range(len(nums)):
+            difference = target - nums[digit]
+            if difference not in hashmap:
+                hashmap[nums[digit]] = digit
+            else:
+                output[0] = hashmap[difference]
+                output[1] = digit
+                return output
