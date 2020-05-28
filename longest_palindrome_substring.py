3b8837faf1026476508cb53657da1cfc82f906da
Swarup Hegde <swaruphegde@gmail.com>
2020-05-28 18:32:03 +0530

    fix: m->max

--- longest_palindrome_substring.py
+++ longest_palindrome_substring.py
@@ -2,12 +2,12 @@ class Solution:
     def longestPalindrome(self, input_string: str) -> str:
         if len(input_string) == 1:
             return input_string
-        m = []
+        max = []
         for i in range(len(input_string)):
             for j in range(i+1, len(input_string)+1):
                 substring = input_string[i:j]
                 if substring == substring[::-1] and len(substring) >= len(m):
-                    m = substring
+                    max = substring
         output = ''.join(m)
         return output
                 
