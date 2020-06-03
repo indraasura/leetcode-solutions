71aa05d6aa2011dd26573bc9a3bf8f6d955b699a
Swarup Hegde <swaruphegde@gmail.com>
2020-06-03 17:35:56 +0530

    fix: atoi.py

--- atoi.py
+++ atoi.py
@@ -1,26 +1,33 @@
-class Solution: 
-    def __init__(self, s: str) -> int:
-        MAX_INT = 2**31 - 1
-        MIN_INT = -2**31
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        i = 0 
         res = 0
-        i = 0
+        count = 0
         negative = 1
+        MAX_INT = 2 ** 31 - 1
+        MIN_INT = -2 ** 31
         # whitespace check
-        while i < len(s) and s[i] == ' ':
+        while i < len(str) and str[i] == ' ':
             i += 1
-        # +/- symbol check : no loop as only one time check
-        if i < len(s) and s[i] == '-':
+        # +/i check
+        if i < len(str) and str[i] == '-':
             negative = -1
             i += 1
-        if i < len(s) and s[i] == '+':
+            count += 1
+        if i < len(str) and str[i] == '+':
             i += 1
+            count += 1
+        if count > 1:
+            return 0
+        else: 
         # number check
-        checker = set('0123456789')
-        while i < len(s) and s[i] in checker:
-            res = res*10 + int(s[i])
-            i += 1
-        res = res * negative
-        if res < 0:
-            return max(res, MIN_INT)
-        else:
-            return min(res, MAX_INT)
+            checker = set('0123456789')
+            while i < len(str) and str[i] in checker:
+                res = res*10 + int(str[i])
+                i += 1
+            res *= negative
+            if res < 0:
+                return max(res, MIN_INT)
+            else:
+                return min(res, MAX_INT)
+        
